e688f91efedf4cac970c848b81500f9f08eb5ed0
Jozsef Geng <gengjozsef86@gmail.com>
2019-11-29 09:54:53 +0100

    add QuoteCollectionTransfer param

--- src/FondOfSpryker/Zed/CheckoutRestApiExtension/Dependency/Plugin/ChildQuoteMapperPluginInterface.php
+++ src/FondOfSpryker/Zed/CheckoutRestApiExtension/Dependency/Plugin/ChildQuoteMapperPluginInterface.php
@@ -2,6 +2,7 @@
 
 namespace FondOfSpryker\Zed\CheckoutRestApiExtension\Dependency\Plugin;
 
+use Generated\Shared\Transfer\QuoteCollectionTransfer;
 use Generated\Shared\Transfer\QuoteTransfer;
 use Generated\Shared\Transfer\RestCheckoutRequestAttributesTransfer;
 
@@ -14,12 +15,15 @@ interface ChildQuoteMapperPluginInterface
      * @api
      *
      * @param \Generated\Shared\Transfer\RestCheckoutRequestAttributesTransfer $restCheckoutRequestAttributesTransfer
+     * @param \Generated\Shared\Transfer\QuoteCollectionTransfer $quoteCollectionTransfer
      * @param \Generated\Shared\Transfer\QuoteTransfer $childQuoteTransfer
      * @param \Generated\Shared\Transfer\QuoteTransfer $quoteTransfer
+     *
      * @return \Generated\Shared\Transfer\QuoteTransfer
      */
     public function map(
         RestCheckoutRequestAttributesTransfer $restCheckoutRequestAttributesTransfer,
+        QuoteCollectionTransfer $quoteCollectionTransfer,
         QuoteTransfer $childQuoteTransfer,
         QuoteTransfer $quoteTransfer
     ): QuoteTransfer;
